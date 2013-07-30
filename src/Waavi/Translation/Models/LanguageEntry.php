6600a64cf290dd852ead7d0702c444cfb86ebabb
William <william@amovens.com>
2013-07-30 18:43:03 +0200

    updateText and mark translations as unstable.
    Signed-off-by: William <william@amovens.com>

--- src/Waavi/Translation/Models/LanguageEntry.php
+++ src/Waavi/Translation/Models/LanguageEntry.php
@@ -25,4 +25,27 @@ class LanguageEntry extends Model {
   {
   	return $this->belongsTo('Waavi\Translation\Models\Language');
   }
+
+  /**
+   *  Update the text. In case the second argument is true, then all translations for this entry will be flagged as unstable.
+   *  @param  string   $text
+   *  @param  boolean  $isDefault
+   *  @return boolean
+   */
+  public function updateText($text, $isDefault = false)
+  {
+    $this->text = $text;
+    if ($this->save()) {
+      if ($isDefault) {
+        LanguageEntry::where('namespace', '=', $this->namespace)
+          ->where('group', '=', $this->group)
+          ->where('item', '=', $this->item)
+          ->where('language_id', '!=', $this->language_id)
+          ->update(array('unstable' => '1'));
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
 }

3478497a0cee79240f952e73922a95d318788d0c
William Wallace <william@waavi.com>
2016-06-02 12:48:33 +0200

    Increase locale length to 10 chars. See issue #42

---
+++ database/migrations/2016_06_02_124154_increase_locale_length.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class IncreaseLocaleLength extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('translator_languages', function ($table) {
+            $table->string('locale', 10)->change();
+        });
+        Schema::table('translator_translations', function ($table) {
+            $table->string('locale', 10)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('translator_translations');
+    }
+
+}

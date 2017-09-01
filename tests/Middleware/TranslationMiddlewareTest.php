731392a17cd1b3396fc76476da18e4d2f2151aa4
William Wallace <william@waavi.com>
2017-09-01 16:49:03 +0200

    Added test to check that POST requests load language from session.

--- tests/Middleware/TranslationMiddlewareTest.php
+++ tests/Middleware/TranslationMiddlewareTest.php
@@ -1,5 +1,7 @@
-<?php namespace Waavi\Translation\Test\Middleware;
+<?php
+namespace Waavi\Translation\Test\Middleware;
 
+use Waavi\Translation\Repositories\TranslationRepository;
 use Waavi\Translation\Test\TestCase;
 
 class TranslationMiddlewareTest extends TestCase
@@ -100,5 +102,40 @@ class TranslationMiddlewareTest extends TestCase
         $this->assertEquals(302, $response->getStatusCode());
         $this->assertTrue($response->headers->has('location'));
         $this->assertEquals('http://localhost/api/v1/en/ca/locale', $response->headers->get('location'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_keeps_locale_in_post_requests_with_no_locale_set()
+    {
+        $translationRepository = \App::make(TranslationRepository::class);
+        $trans                 = $translationRepository->create([
+            'locale'    => 'en',
+            'namespace' => '*',
+            'group'     => 'welcome',
+            'item'      => 'title',
+            'text'      => 'Welcome',
+        ]);
+
+        $trans = $translationRepository->create([
+            'locale'    => 'es',
+            'namespace' => '*',
+            'group'     => 'welcome',
+            'item'      => 'title',
+            'text'      => 'Bienvenido',
+        ]);
+
+        $this->call('GET', '/es');
+        $response   = $this->call('POST', '/welcome');
+        $statusCode = $response->getStatusCode();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('Bienvenido', $response->getContent());
+
+        $this->call('GET', '/en');
+        $response   = $this->call('POST', '/welcome');
+        $statusCode = $response->getStatusCode();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('Welcome', $response->getContent());
     }
 }

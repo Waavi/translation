89d2ae5cc42f54d5e6308a8c4b75e219fd83a667
Max Slotov <maxslotov@icloud.com>
2020-03-02 09:08:06 +0200

    Add PHPDoc methods to Facades.

--- src/Facades/TranslationCache.php
+++ src/Facades/TranslationCache.php
@@ -2,6 +2,15 @@
 
 use \Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static bool has($locale, $group, $namespace)
+ * @method static mixed get($locale, $group, $namespace)
+ * @method static void put($locale, $group, $namespace, $content, $minutes)
+ * @method static void flush($locale, $group, $namespace)
+ * @method static void flushAll($locale, $group, $namespace)
+ *
+ * @see \Waavi\Translation\Cache\CacheRepositoryInterface
+ */
 class TranslationCache extends Facade
 {
     /**

b4f1d66cbbfba643f16aeda3d004529a974bcecf
Max <maxslotov@icloud.com>
2020-03-09 20:10:11 +0200

    Add PHPDoc methods to Facades. (#170)

--- src/Facades/UriLocalizer.php
+++ src/Facades/UriLocalizer.php
@@ -2,6 +2,14 @@
 
 use \Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string localeFromRequest($segment = 0)
+ * @method static string localize($url, $locale, $segment = 0)
+ * @method static string|null getLocaleFromUrl($url, $segment = 0)
+ * @method static string cleanUrl($url, $segment = 0)
+ *
+ * @see \Waavi\Translation\UriLocalizer
+ */
 class UriLocalizer extends Facade
 {
     /**
